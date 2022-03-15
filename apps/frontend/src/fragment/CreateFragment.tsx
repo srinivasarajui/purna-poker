5d87c50708ce5e5bb2cbbb7c78aa5eb041699c93
srinivasarajui <srinivarajui@gmail.com>
2022-03-15 12:04:32 +0530

    added code

--- apps/frontend/src/fragment/CreateFragment.tsx
+++ apps/frontend/src/fragment/CreateFragment.tsx
@@ -19,23 +19,30 @@ interface CreateGameState {
   userName: string;
   gameName: string;
   voitingSystem: string;
+  isUserNameValid: boolean;
+  isGameNameValid: boolean;
+  isVoitingSystemValid: boolean;
   isButtonDisabled: boolean;
 }
 
 
 // Our reducer function that uses a switch statement to handle our actions
 function CreateGameReducer(state: CreateGameState, action: CreateGameAction) {
-  const { type, payload } = action;
+  let { type, payload } = action;
+  payload = payload.trim();
   const nextState: CreateGameState = { ...state };
   switch (type) {
     case CreateGameActions.CHANGE_USER_NAME:
       nextState.userName = payload;
+      nextState.isUserNameValid = payload.length !== 0;
       break;
     case CreateGameActions.CHANGE_GAME_NAME:
       nextState.gameName = payload;
+      nextState.isGameNameValid = payload.length !== 0;
       break
     case CreateGameActions.CHANGE_VOITING_SYSTEM:
      nextState.voitingSystem = payload;
+      nextState.isVoitingSystemValid = payload.length !== 0;
   }
   nextState.isButtonDisabled =  !(nextState.userName.trim().length > 0 && nextState.gameName.trim().length > 0 && nextState.voitingSystem.length > 0);
   return nextState;
@@ -56,6 +63,9 @@ export function CreateFragment() {
     userName: '',
     gameName: '',
     voitingSystem: '',
+    isUserNameValid: true,
+    isGameNameValid: true,
+    isVoitingSystemValid: true,
     isButtonDisabled: true,
   });
   const handleCreateGame = async () => {
@@ -70,15 +80,21 @@ export function CreateFragment() {
         Create a new game
       </Heading>
       <VStack space={3} mt="5">
-        <FormControl>
+        <FormControl isRequired isInvalid={!state.isUserNameValid}>
           <FormControl.Label >User name</FormControl.Label>
           <Input variant="outline" value={state.userName} onChangeText={itemValue => dispatch({type: CreateGameActions.CHANGE_USER_NAME,  payload:itemValue })}  />
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              User name is required.
+            </FormControl.ErrorMessage>
         </FormControl>
-        <FormControl>
+        <FormControl isRequired isInvalid={!state.isGameNameValid}>
           <FormControl.Label>Game Name</FormControl.Label>
           <Input variant="outline"  value={state.gameName} onChangeText={itemValue => dispatch({type: CreateGameActions.CHANGE_GAME_NAME,  payload:itemValue })} />
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              Game name is required.
+            </FormControl.ErrorMessage>
         </FormControl>
-        <FormControl>
+        <FormControl isRequired isInvalid={!state.isVoitingSystemValid}>
           <FormControl.Label>Voting System</FormControl.Label>
           <Select accessibilityLabel="Voting System" placeholder="Choose Voting System" _selectedItem={{
             bg: "teal.600",
@@ -94,6 +110,9 @@ export function CreateFragment() {
             }
 
           </Select>
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+          Voting System  is required.
+            </FormControl.ErrorMessage>
         </FormControl>
         <Button mt="2"  isDisabled={state.isButtonDisabled} onPress={handleCreateGame} >
           Create a new Game
--- apps/frontend/src/fragment/JoinFragment.tsx
+++ apps/frontend/src/fragment/JoinFragment.tsx
@@ -1,31 +1,84 @@
-import { Box, Heading, VStack, FormControl, Input, Button, Center, HStack, Text } from "native-base";
+import { Box, Heading, VStack, FormControl, Input, Button, Center, HStack, Text, WarningOutlineIcon } from "native-base";
 
 import React from "react";
+enum JoinGameActions {
+  CHANGE_USER_NAME = 'changeUserName',
+  CHANGE_GAME_CODE = 'changeGameCode',
+  CHANGE_ADMIN_CODE = 'changeAdminCode',
 
+}
+interface JoinGameAction {
+  type: JoinGameActions;
+  payload: string;
+}
+
+interface JoinGameState {
+  userName: string;
+  gameCode: string;
+  adminCode: string;
+  isUserNameValid: boolean;
+  isGameCodeValid: boolean;
+  isButtonDisabled: boolean;
+}
+
+function JoinGameReducer(state: JoinGameState, action: JoinGameAction) {
+  const { type, payload } = action;
+  const nextState: JoinGameState = { ...state };
+  switch (type) {
+    case JoinGameActions.CHANGE_USER_NAME:
+      nextState.userName = payload;
+      nextState.isUserNameValid= payload.length === 0 ;
+
+      break;
+    case JoinGameActions.CHANGE_GAME_CODE:
+      nextState.gameCode = payload;
+      nextState.isGameCodeValid= !payload.match(/^[0-9a-fA-F]{24}$/);
+      break
+    case JoinGameActions.CHANGE_ADMIN_CODE:
+     nextState.adminCode = payload;
+  }
+  nextState.isButtonDisabled =  !(nextState.userName.trim().length > 0 && nextState.gameCode.trim().length > 0 && nextState.gameCode.match(/^[0-9a-fA-F]{24}$/));
+  return nextState;
+}
 export function JoinFragment(){
+  const [state, dispatch] = React.useReducer(JoinGameReducer, {
+    userName: '',
+    gameCode: '',
+    adminCode: '',
+    isUserNameValid: false,
+    isGameCodeValid: false,
+    isButtonDisabled: true,
+  });
   return (<Center w="100%">
     <Box  p="2" w="90%"  py="8">
       <Heading size="lg" fontWeight="semibold">
         Join an existing game
       </Heading>
       <VStack space={3} mt="5">
-        <FormControl>
+        <FormControl isRequired isInvalid={state.isUserNameValid}>
           <FormControl.Label><Text>User name</Text></FormControl.Label>
-          <Input />
+          <Input value={state.userName} onChangeText={itemValue => dispatch({type: JoinGameActions.CHANGE_USER_NAME,  payload:itemValue })}/>
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              User name is required.
+            </FormControl.ErrorMessage>
         </FormControl>
-        <FormControl>
+        <FormControl isRequired isInvalid={state.isGameCodeValid}>
           <FormControl.Label>Game code</FormControl.Label>
-          <Input />
+          <Input value={state.gameCode} onChangeText={itemValue => dispatch({type: JoinGameActions.CHANGE_GAME_CODE,  payload:itemValue })}/>
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              Game code is not in correct format.
+            </FormControl.ErrorMessage>
         </FormControl>
         <FormControl>
 
           <FormControl.Label>Admin Code(optional)</FormControl.Label>
-          <Input type="password" />
+          <Input type="password" value={state.adminCode} onChangeText={itemValue => dispatch({type: JoinGameActions.CHANGE_ADMIN_CODE,  payload:itemValue })}/>
 
         </FormControl>
-        <Button mt="2" >
+        <Button mt="2"  isDisabled={state.isButtonDisabled} >
           Go to Game
         </Button>
+
 
       </VStack>
     </Box>
